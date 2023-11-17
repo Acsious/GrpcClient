dadcf71f79d8e49683a1ea5aea10240c559eb26e
Acsious <ulchenko.f@mail.ru>
2023-11-17 22:21:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest() { Name = "World" });
+        Console.WriteLine("Hello " + reply.Message);
+        Console.ReadKey();
+    }
+}
